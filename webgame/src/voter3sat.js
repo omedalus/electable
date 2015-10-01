0b8e88d00fc4d81f90f2ed08bb85123ad37c468b
Omedalus <mvol@live.com>
2015-09-30 21:19:31 -0400

    Still fixing jsfiddle indentation messup

--- webgame/src/voter3sat.js
+++ webgame/src/voter3sat.js
@@ -12,137 +12,137 @@ voter3sat.service('GameService', ['$rootScope', 'IssueFactory', function($rootSc
   };
         
   var generateVoters = function(issues, winningPlatform) {
-      var voters = [];
+    var voters = [];
+    
+    // We construct a set of voters by exhaustively iterating through the space
+    // of possible platforms. If a platform is accepted by all voters, and it *isn't*
+    // our desired winning platform, then we add a voter who would reject that platform.
+    
+    var winningbitmask = 0;
+    var issuekeys = [];
+    _.each(issues, function(issue, issuekey) {
+      issuekeys[issue.index] = issuekey;
+      if (winningPlatform[issuekey]) {
+        winningbitmask |= 1 << issue.index;
+      }
+    });
       
-      // We construct a set of voters by exhaustively iterating through the space
-      // of possible platforms. If a platform is accepted by all voters, and it *isn't*
-      // our desired winning platform, then we add a voter who would reject that platform.
-      
-      var winningbitmask = 0;
-      var issuekeys = [];
-      _.each(issues, function(issue, issuekey) {
-          issuekeys[issue.index] = issuekey;
-          if (winningPlatform[issuekey]) {
-              winningbitmask |= 1 << issue.index;
-          }
-      });
-      
-      var numIssues = _.size(issues);
-      var platformbitsMax = 1 << numIssues;
-      for (var platformbits = 0; platformbits < platformbitsMax; platformbits++) {
-          if (platformbits === winningbitmask) {
-              continue;
-          }
-          
-          var allVotersAccept = _.all(voters, function(voter) {
-              // Does this voter accept this platform?
-              return _.any(voter.opinions, function(opinion, issuekey) {
-                  return opinion === !!((1 << issues[issuekey].index) & platformbits);
-              });
-          });
-          
-          if (!allVotersAccept) {
-              continue;
-          }
-
-          // All voters accept this platform, but this platform isn't the 
-          // desired one. Add a rejecting voter.
-          
-          var voter = {
-              opinions: {},
-              imageOffset: {
-                  x: Math.floor(Math.random() * 5),
-                  y: Math.floor(Math.random() * 4)
-              }
-          };
-          
-          // Get all the issues on which this platform differs from the desired one.
-          var issuesdifferkeys = [];
-          for (var issuebit = 0; issuebit < numIssues; issuebit++) {
-              var issuemask = 1 << issuebit;
-              if ((winningbitmask & issuemask) !== (platformbits & issuemask)) {
-                  issuesdifferkeys.push(issuekeys[issuebit]);
-              }
-          }
-          
-          // Pick one issue at random on which this platform differs from the desired one.
-          // Make this voter agree with the desired platform on this issue (and disagree
-          // with the current platform).
-          var differkey = issuesdifferkeys[Math.floor(Math.random() * issuesdifferkeys.length)];
-          voter.opinions[differkey] = winningPlatform[differkey];
-          
-          // Pick more issues to add to this voter's opinions, and set them all to the opposite
-          // of the current platform. This will make it impossible for this voter to accept
-          // this platform, which will rule it out as a possible solution (because it's not
-          // equal to the desired platform).
-          while (_.size(voter.opinions) < 3) {
-              var keysWeHaventUsedYet = _.difference(_.keys(issues), _.keys(voter.opinions));
-            var issuekey = keysWeHaventUsedYet[Math.floor(Math.random() * keysWeHaventUsedYet.length)];
-              var issue = issues[issuekey];
-              voter.opinions[issuekey] = !(platformbits & (1 << issue.index));
-          }
-          
-          voter.sortedIssueKeys = _.chain(voter.opinions).
-            keys().
-            sortBy(function(issuekey) {
-                return issues[issuekey].index;
-              }).
-              value();
-          
-          voters.push(voter);
+    var numIssues = _.size(issues);
+    var platformbitsMax = 1 << numIssues;
+    for (var platformbits = 0; platformbits < platformbitsMax; platformbits++) {
+      if (platformbits === winningbitmask) {
+        continue;
       }
       
-      voters = _.sortBy(voters, function(voter) {
-          // Generate a sortkey for each voter.
-          // The sortkey consists of a code for the voter's pet issues,
-          // followed by a code for the voter's stances on those issues.
-          var issuesortkey = '';
-          var opinionsortkey = '';
-          
-          _.chain(issues).
-            keys().
-            sortBy(function(issuekey) {
-                  return issues[issuekey].index;
-              }).
-            each(function(issuekey) {
-                if (issuekey in voter.opinions) {
-                      issuesortkey += '1';
-                      opinionsortkey += voter.opinions[issuekey] ? '1' : '0';
-                  } else {
-                      issuesortkey += '0';
-                  }
-            });
-
-          var sortkey = issuesortkey + '-' + opinionsortkey;
-          return sortkey;
+      var allVotersAccept = _.all(voters, function(voter) {
+        // Does this voter accept this platform?
+        return _.any(voter.opinions, function(opinion, issuekey) {
+          return opinion === !!((1 << issues[issuekey].index) & platformbits);
+        });
       });
-      return voters;
+      
+      if (!allVotersAccept) {
+        continue;
+      }
+
+      // All voters accept this platform, but this platform isn't the 
+      // desired one. Add a rejecting voter.
+      
+      var voter = {
+        opinions: {},
+        imageOffset: {
+          x: Math.floor(Math.random() * 5),
+          y: Math.floor(Math.random() * 4)
+        }
+      };
+        
+      // Get all the issues on which this platform differs from the desired one.
+      var issuesdifferkeys = [];
+      for (var issuebit = 0; issuebit < numIssues; issuebit++) {
+        var issuemask = 1 << issuebit;
+        if ((winningbitmask & issuemask) !== (platformbits & issuemask)) {
+          issuesdifferkeys.push(issuekeys[issuebit]);
+        }
+      }
+        
+      // Pick one issue at random on which this platform differs from the desired one.
+      // Make this voter agree with the desired platform on this issue (and disagree
+      // with the current platform).
+      var differkey = issuesdifferkeys[Math.floor(Math.random() * issuesdifferkeys.length)];
+      voter.opinions[differkey] = winningPlatform[differkey];
+      
+      // Pick more issues to add to this voter's opinions, and set them all to the opposite
+      // of the current platform. This will make it impossible for this voter to accept
+      // this platform, which will rule it out as a possible solution (because it's not
+      // equal to the desired platform).
+      while (_.size(voter.opinions) < 3) {
+        var keysWeHaventUsedYet = _.difference(_.keys(issues), _.keys(voter.opinions));
+        var issuekey = keysWeHaventUsedYet[Math.floor(Math.random() * keysWeHaventUsedYet.length)];
+        var issue = issues[issuekey];
+        voter.opinions[issuekey] = !(platformbits & (1 << issue.index));
+      }
+          
+      voter.sortedIssueKeys = _.chain(voter.opinions).
+        keys().
+        sortBy(function(issuekey) {
+          return issues[issuekey].index;
+        }).
+        value();
+      
+      voters.push(voter);
+    }
+    
+    voters = _.sortBy(voters, function(voter) {
+      // Generate a sortkey for each voter.
+      // The sortkey consists of a code for the voter's pet issues,
+      // followed by a code for the voter's stances on those issues.
+      var issuesortkey = '';
+      var opinionsortkey = '';
+      
+      _.chain(issues).
+        keys().
+        sortBy(function(issuekey) {
+              return issues[issuekey].index;
+          }).
+        each(function(issuekey) {
+            if (issuekey in voter.opinions) {
+                  issuesortkey += '1';
+                  opinionsortkey += voter.opinions[issuekey] ? '1' : '0';
+              } else {
+                  issuesortkey += '0';
+              }
+        });
+
+      var sortkey = issuesortkey + '-' + opinionsortkey;
+      return sortkey;
+    });
+    return voters;
   };
 
   // Sanity-check to ensure that there is only one winning platform.
   var checkVictoryConditions = function(winningbitmask) {
-      var numOtherWinning = 0;
-      var origPlatform = GameService.platform;
-      GameService.platform = {};
-      
-      var numIssues = _.size(GameService.issues);
-      var platformbitsMax = 1 << numIssues;
-      for (var platformbits = 0; platformbits < platformbitsMax; platformbits++) {
-          if (platformbits === winningbitmask) {
-              continue;
-          }
-          
-          _.each(GameService.issues, function(issue, issuekey) {
-              GameService.platform[issuekey] = !!(platformbits & (1 << issue.index));
-          });
-          
-          if (GameService.isPlatformWinning()) {
-              numOtherWinning++;
-          }
+    var numOtherWinning = 0;
+    var origPlatform = GameService.platform;
+    GameService.platform = {};
+    
+    var numIssues = _.size(GameService.issues);
+    var platformbitsMax = 1 << numIssues;
+    for (var platformbits = 0; platformbits < platformbitsMax; platformbits++) {
+      if (platformbits === winningbitmask) {
+        continue;
       }
       
-      GameService.platform = origPlatform;
-      return numOtherWinning;
+      _.each(GameService.issues, function(issue, issuekey) {
+        GameService.platform[issuekey] = !!(platformbits & (1 << issue.index));
+      });
+      
+      if (GameService.isPlatformWinning()) {
+        numOtherWinning++;
+      }
+    }
+    
+    GameService.platform = origPlatform;
+    return numOtherWinning;
   };    
   
   var init = function(numIssues) {
